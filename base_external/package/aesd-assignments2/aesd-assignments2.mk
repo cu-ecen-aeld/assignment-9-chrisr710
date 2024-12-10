8dff96625309ed25c0b5b7be85bdf1e2a94b8ff0
christopher rowland <christopher.o.rowland@colorado.edu>
2024-12-10 14:09:19 -0700

    new-app

---
+++ base_external/package/aesd-assignments2/aesd-assignments2.mk
@@ -0,0 +1,34 @@
+
+##############################################################
+#
+# AESD-ASSIGNMENTS2
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD_ASSIGNMENTS2_VERSION = 'c4d820c'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_ASSIGNMENTS2_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-chrisr710.git'
+AESD_ASSIGNMENTS2_SITE_METHOD = git
+AESD_ASSIGNMENTS2_GIT_SUBMODULES = YES
+
+define AESD_ASSIGNMENTS2_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+endef
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define AESD_ASSIGNMENTS2_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	#cd $(TARGET_DIR)
+	#mkdir ./etc/init.d/rc0.d
+	#mkdir ./etc/init.d/rc5.d
+	#mkdir ./etc/init.d/rc6.d
+	#ln -s ./etc/init.d/aesdsocket-start-stop ./etc/init.d/rc5.d/S99aesdsocket
+	#ln -s ./etc/init.d/aesdsocket-start-stop ./etc/init.d/rc0.d/K01aesdsocket
+	#ln -s ./etc/init.d/aesdsocket-start-stop ./etc/init.d/rc6.d/K01aesdsocket
+endef
+
+$(eval $(generic-package))

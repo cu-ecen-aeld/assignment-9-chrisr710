f34494ca7107ddcac9983653902e499acf501ac7
christopher rowland <christopher.o.rowland@colorado.edu>
2024-12-10 17:50:10 -0700

    updated-commit

--- base_external/package/aesd-assignments2/aesd-assignments2.mk
+++ base_external/package/aesd-assignments2/aesd-assignments2.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS2_VERSION = 'c4d820c'
+AESD_ASSIGNMENTS2_VERSION = '66e2f9b'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
